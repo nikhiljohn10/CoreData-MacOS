ebd70c5263f7fbea12d4dcb7d49ab8d5f34969e2
Nikz Jon <me@nikz.in>
2020-04-22 06:09:38 +0530

    Initial Commit

---
+++ CoreData-MacOS/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CoreData-MacOS
+//
+//  Created by Nikhil John on 22/04/20.
+//  Copyright © 2020 Nikz.in. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
